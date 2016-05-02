1ac6b8d5e037d5e006ffbe10fbea0f70163d3ed6
Ricardo Marquez <ricardo.mar.ferr@gmail.com>
2016-05-02 08:49:40 -0600

    hice un case

--- index.php
+++ index.php
@@ -6,4 +6,15 @@ int b;
 int r=a+b;
 echo r;
 
+$hoy ="martes";
+switch($hoy){
+   case "lunes": echo "Hoy es lunes";break;
+   case "martes": echo "Hoy es martes";break;
+   case "miercoles": echo "Hoy es miercoles";break;
+   case "jueves": echo "Hoy es jueves";break;
+   case "viernes": echo "Hoy es viernes";break;
+   default:
+      echo "NO hay dia ";
+         
+}
 ?>
